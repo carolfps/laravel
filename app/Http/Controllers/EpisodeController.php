dbb02dcac40f1c1d51a49297d66a481ccf6e5972
carolpfs <Digital@DH-A5-03.dhbrasil.digitalhouse.com>
2020-03-06 11:50:29 -0300

    deletando editando e inserindo dados na tabela

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -9,13 +9,36 @@ use App\Episode;
 class EpisodeController extends Controller
 {
     public function index(){
-        $episodes = Episode::all();
-        $episode = Episode::find(1);//busca o id=1
+        //$episodes = Episode::all();
+        //$episode = Episode::find(1);//busca o id=1
         //$episodes_com_query = Episode::query()->whereIn('id', [1,2])->get();
-        $episodes_com_query = Episode::query()->whereIn('id', [1,2])->first();//só retorna o primeiro da consulta
+        //$episodes_com_query = Episode::query()->whereIn('id', [1,2])->first();//só retorna o primeiro da consulta
         
-        dd($episodes_com_query->toArray());
+        //dd($episodes_com_query->toArray());
         //dd($episode->toArray()); //só mostra os dados da tabela
         //dd($episodes);
+
+        //inserindo uma linha na tabela episodes
+        // $episode = new Episode();
+        // $episode -> title = "Mr. Robot";
+        // $episode -> number = "456";
+        // $episode -> rating = "10";
+        // $episode -> release_date ="2020-03-06 00:00:00";
+        // $episode->save();
+
+        //mostrando na tela os dados do episodio que acabamos de inserir
+        // $episode = Episode::find(58);
+        // dd($episode->toArray());
+
+        //Editando o título episodio com id=1
+        // $episode = Episode::find(1);
+        // $episode -> title = "--Winter Is Coming --";
+        // $episode->save();
+
+        //Deletando uma linha da tabela episodes 
+        $episode = Episode::find(58);
+        $episode->delete();
+
     }
+
 }
